c020e9de2ee174a9f9363b5d54dd8e1456b34130
Ben Turner <ben@gist.com>
2010-07-20 15:30:21 -0700

    Adding view helpers for generating an HTML profiler report.

--- init.rb
+++ init.rb
@@ -1,3 +1,6 @@
 # Include hook code here
+require 'profiler_view_helper'
 
 ActiveRecord::ConnectionAdapters::AbstractAdapter.send :include, ActiveRecordProfiler
+
+ActionView::Base.send :include, ProfilerViewHelper
