ca096796285f06204c3a9516c7f43da3e6e8caad
Ben Turner <ben@octodot.com>
2015-01-24 13:41:03 -0800

    Better defaults

--- config/initializers/default_config.rb
+++ config/initializers/default_config.rb
@@ -1,8 +1,8 @@
 ActiveRecordProfiler.stats_flush_period = 1.hour
 ActiveRecordProfiler.profile_dir = Rails.root.join("log", "profiler_data")
 ActiveRecordProfiler.sql_ignore_pattern = 
-  /^(SHOW FIELDS |SET SQL_AUTO_IS_NULL|SET NAMES |EXPLAIN |BEGIN|COMMIT|PRAGMA )/i
-ActiveRecordProfiler.app_path_pattern = Regexp.new(Regexp.quote("/app/"))
+  /^(SHOW (:?FULL )?FIELDS |SET SQL_AUTO_IS_NULL|SET NAMES |EXPLAIN |BEGIN|COMMIT|PRAGMA )/i
+ActiveRecordProfiler.app_path_pattern = Regexp.new(Regexp.quote("#{Rails.root.expand_path}/") + "(:?app|lib|vendor)/")
 ActiveRecordProfiler.trim_root_path = "#{Rails.root.expand_path}/"
 ActiveRecordProfiler.profile_self = false
 ActiveRecordProfiler.link_location = false
