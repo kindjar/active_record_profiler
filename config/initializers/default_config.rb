9cd352c0799a71765482cf3b81557481bd33ab14
Ben Turner <ben@octodot.com>
2015-01-24 13:53:54 -0800

    Apply settings in initializer after configuration

--- config/initializers/default_config.rb
+++ config/initializers/default_config.rb
@@ -6,10 +6,3 @@ ActiveRecordProfiler.app_path_pattern = Regexp.new(Regexp.quote("#{Rails.root.ex
 ActiveRecordProfiler.trim_root_path = "#{Rails.root.expand_path}/"
 ActiveRecordProfiler.profile_self = false
 ActiveRecordProfiler.link_location = false
-
-# Copy module settings to collector class
-[:stats_flush_period, :profile_dir, :sql_ignore_pattern, :app_path_pattern, 
-    :trim_root_path, :profile_self].each do |config_item|
-  ActiveRecordProfiler::Collector.send("#{config_item}=".to_sym, 
-      ActiveRecordProfiler.send(config_item))
-end
--- lib/active-record-profiler/engine.rb
+++ lib/active-record-profiler/engine.rb
@@ -5,5 +5,15 @@ module ActiveRecordProfiler
     rake_tasks do
       load 'active-record-profiler/tasks.rake'
     end
+
+    initializer "active_record_profiler.apply_default" do
+      # Copy module settings to collector class
+      [:stats_flush_period, :profile_dir, :sql_ignore_pattern, 
+          :app_path_pattern, :trim_root_path, :profile_self
+      ].each do |config_item|
+        ActiveRecordProfiler::Collector.send("#{config_item}=".to_sym, 
+            ActiveRecordProfiler.send(config_item))
+      end
+    end
   end
 end
